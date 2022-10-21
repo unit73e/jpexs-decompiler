fbf8b9df78a8e0eea6381006414bc48a33280e62
unit73e <unit73e@gmail.com>
2022-10-21 23:07:43 +0100

    Remove unnecessary files
    The goal of this fork is to move away from Ant, using Gradle instead,
    so it's best to remove any unnecessary files, including:
    - Ant files. Replaced by Gradle.
    - Netbeans files. Gradle is IDE agnostic.
    - Installer and run scripts. Gradle has its own solutions.
    - Travis. To avoid confusion with the original project.

--- git_eol_normalization.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-git add . -u
-git commit -m "Saving files before refreshing line endings"
-git rm --cached -r .
-git reset --hard
-git add .
-set +e
-git commit -m "Normalize all the line endings"
--- iggy-extract.bat
+++
@@ -1,2 +0,0 @@
-@java -cp lib/ffdec_lib.jar com.jpexs.decompiler.flash.iggy.conversion.IggyToSwfConvertor "%1" "%2"
-@pause
